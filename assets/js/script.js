302f02539b9ef7ff3feea0c53cf0edca702f56c9
Rafi Kadir <rafikadir2000@gmail.com>
2022-08-27 01:16:17 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+// Back to top
+$(document).ready(function(){
+	$(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+});
+
+ $('#back-to-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 400);
+    return false;
+});
+
+// Navbar Shrink
+$(window).on("scroll",function(){
+    $(this).scrollTop()>100?$(".main-nav").addClass("nav-shrink"):$(".main-nav").removeClass("nav-shrink");
+ });
+
+
+
+
+
+
+
+
